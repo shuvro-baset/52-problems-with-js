ae9696a89381c418dc7a083d85f603586ddd1ae4
shuvro baset <md.abdul.baset75@gmail.com>
2021-08-01 04:18:08 +0600

    even odd done

---
+++ even-odd.js
@@ -0,0 +1,19 @@
+let num = 20
+if (num%2 == 0) {
+    console.log(num, ": is a even number");
+}
+else {
+    console.log(num, ": is a odd number");
+}
+
+
+function evenOrOdd(num) { //for string length take string
+    if (num % 2 == 0) {//and replace numm with string.length here
+      return "even";
+    } else {
+      return "odd";
+    }
+  }
+console.log(evenOrOdd(21))
+
+
